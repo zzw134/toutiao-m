a07b75388404103e3c2b50d10ac6eb2bd8cff154
zzw <1805325296@qq.com>
2021-09-07 14:13:30 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.css'
+
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
